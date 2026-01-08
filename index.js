47fe5ee4fcb8e68235b21157efbc76a5d34ed342
ankitsyadav <ankitsinghyadav009@gmail.com>
2026-01-08 10:24:36 +0530

    dsdbjs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require("express");
+
+const app=express();
+
+
+app.get("/",(req,res)=>{
+    return res.status(200).send("success ok !!!");
+})
+
+app.listen(3000,()=>{
+    console.log("server listening on port 3000");
+})
